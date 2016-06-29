283b8940675013b8cb84155ab9bc35bb8b8a4ea8
Bruno Paulino <brunojppb@gmail.com>
2016-06-29 19:27:11 -0300

    Build Carthage

---
+++ Carthage/Build/iOS/VFRReader.framework/Headers/VFRReader.h
@@ -0,0 +1,19 @@
+//
+//  VFRReader.h
+//  VFRReader
+//
+//  Created by Bruno Paulino on 6/29/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VFRReader.
+FOUNDATION_EXPORT double VFRReaderVersionNumber;
+
+//! Project version string for VFRReader.
+FOUNDATION_EXPORT const unsigned char VFRReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VFRReader/PublicHeader.h>
+
+
